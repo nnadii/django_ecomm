b304a74f1c2f996b92795e4741a8ccc4f18702ac
Chimennadi <nnadichime04@gmail.com>
2024-06-07 19:29:24 +0100

    created user auth

--- userauth/views.py
+++ userauth/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from userauth.forms import UserRegisterForm
 
 # Create your views here.
+
+
+def register_view(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = UserRegisterForm()
+
+    context = {"form": form}
+    return render(request, "userauth/sign-up.html", context)
