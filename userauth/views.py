204f0f8e9780399ec62788bf71a96573de35bc00
Chimennadi <nnadichime04@gmail.com>
2024-06-07 21:54:01 +0100

    templates changed

--- userauth/views.py
+++ userauth/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from userauth.forms import UserRegisterForm
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
 
 # Create your views here.
 
@@ -8,7 +10,14 @@ def register_view(request):
     if request.method == "POST":
         form = UserRegisterForm(request.POST)
         if form.is_valid():
-            form.save()
+            new_user = form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(
+                request, f"Hey {username}, your account was created successfully")
+            new_user = authenticate(username=form.cleaned_data["email"],
+                                    password=form.cleaned_data["password1"])
+            login(request, new_user)
+            return redirect("core:index")
     else:
         form = UserRegisterForm()
 
