71ebff2dd9fd834e98373a5606689510f62d2d3c
Chimennadi <nnadichime04@gmail.com>
2024-06-10 17:50:53 +0100

    Adding postgresql database

--- ecom/settings.py
+++ ecom/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = "ecom.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "ecomm-test",
+        "USER": "masteruser",
+        "PASSWORD": "Chimezie98",
+        "HOST": "ecomm-test.cfm6kouyc7pj.us-east-1.rds.amazonaws.com",
+        "PORT": "5432"
     }
 }
 
